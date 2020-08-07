19f9cb3c8c6d973988ba3e49b97a47e10cb5bb0c
iwh718 <630020826@qq.com>
2020-08-07 09:50:22 +0800

    移除不必要的权限

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # 蝴蝶主页🦋
 Chrome主页插件，百度净化，下载站净化生成当前页面二维码
-
+v1.0.4
 ![预览图](https://b3logfile.com/file/2020/07/image-e9eb6f10.png)
--- js/bg.js
+++ js/bg.js
@@ -1,17 +1,5 @@
 console.log('%c蝴蝶工具箱启动',"background:#1260ff;border-radius:5px;padding:2px 4px ;color:#fff")
-chrome.contextMenus.removeAll();
-chrome.contextMenus.create({
-	title: "打开蝴蝶搜索",
-	onclick: function(){openIndex()}
-});
-chrome.contextMenus.create({
-	title: "查看明文密码",
-	onclick: function(){
-        getCurrentTabId((tabId)=>{
-            chrome.tabs.executeScript(tabId, {code: "document.querySelectorAll('input[type=password]').forEach((e)=>{e.setAttribute('type','text')});"});
-        }) 
-    }
-});
+
 function openIndex(){
     chrome.tabs.create({ url: chrome.runtime.getURL('./Q.html') });
 }
@@ -24,22 +12,7 @@ function getCurrentTabId(callback)
         console.log('tabId:' + tabs[0].id)
 	});
 }
-function notice(str,message){
-    chrome.notifications.create(null, {
-        type: 'basic',
-        iconUrl: './icon.png',
-        title: str,
-        message:message
-    });
-}
-function getHistory(callback){
-    chrome.history.search({
-        'text': '',              // Return every history item....
-        'startTime': 1000*60*60*24*3  // that was accessed less than one week ago.
-      },
-      function(historyItems) {
-          console.log(historyItems)
-      });
-}
+
+
 
 
